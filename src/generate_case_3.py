6e082c9b87b2d5a284016c08c8ba75221122b33c
Callum Atkinson <callum.atkinson@monash.edu>
2026-07-25 14:01:07 +1000

    Update generate_case_3.py

--- src/generate_case_3.py
+++ src/generate_case_3.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-A = np.array([
+import numpy as np
+
+A64 = np.array([
     [3.271428391e8,  1.552948112e9,  9.114228551e7,  4.771992331e6,  8.221551992e5],
     [6.542856782e8,  3.104896224e9,  1.823845710e8,  9.543984662e6,  1.644310398e6],
     [9.814285173e8,  4.657844336e9,  2.735268565e8,  1.431597699e7,  2.466465597e6],
@@ -8,16 +10,21 @@ A = np.array([
      1.610771992331e13, 2.010551992331e13],
 ], dtype=np.float64)
 
-row4 = A[3]
+row4 = A64[3]
 
-factor = 4.1758690198181  # non‑trivial scaling factor
+factor = 4.1758690198181   # non‑trivial, not visually obvious
 
 row5 = row4 * factor
 
-A = np.vstack([A, row5])
+A64 = np.vstack([A64, row5])
+
+A32 = A64.astype(np.float32)
+print("Rank float32:", np.linalg.matrix_rank(A32))
+
 
 b = np.array([1., 2., 3., 4., 5.], dtype=np.float64)
 
 # save to file
-np.save('A3.npy',A)
+np.save('A3_64.npy',A64)
+np.save('A3_64.npy',A32)
 np.save('b3.npy',b)
